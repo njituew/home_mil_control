1bfaa0d3a93278b4a3aa6f3f4c263b13c4dc72d9
Gosha <egorglinnik2@gmail.com>
2025-08-03 13:04:41 +0300

    new logs

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
         logging.StreamHandler(),
-        logging.FileHandler("bot.log", encoding="utf-8")
+        logging.FileHandler("bot.log", encoding="utf-8", mode="a")
     ]
 )
 
