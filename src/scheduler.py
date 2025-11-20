909f2f881ed3c23a60ed07e90672f87800be606e
Gosha <egorglinnik2@gmail.com>
2025-11-20 20:48:24 +0300

    CET to Moscow time

--- src/scheduler.py
+++ src/scheduler.py
@@ -13,27 +13,27 @@ def init_scheduler(bot):
     scheduler = AsyncIOScheduler(timezone=pytz.timezone("Europe/Moscow"))
     scheduler.add_job(
         clear_today_control,
-        CronTrigger(hour=18, minute=30),
+        CronTrigger(hour=19, minute=30),
         id="clear_today_control",
         replace_existing=True,
     )
     scheduler.add_job(
         send_reminder,
-        CronTrigger(hour=18, minute=40),
+        CronTrigger(hour=19, minute=40),
         args=[bot],
         id="send_reminder",
         replace_existing=True,
     )
     scheduler.add_job(
         send_last_chance,
-        CronTrigger(hour=19, minute=5),
+        CronTrigger(hour=20, minute=5),
         args=[bot],
         id="send_last_chance",
         replace_existing=True,
     )
     scheduler.add_job(
         send_daily_report,
-        CronTrigger(hour=19, minute=10),
+        CronTrigger(hour=20, minute=10),
         args=[bot],
         id="send_daily_report",
         replace_existing=True,
